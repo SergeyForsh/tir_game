a5543f4ce4c0a504e16930d53a3ba4af35448d08
SergeyForsh <seforsh@gmail.com>
2025-02-05 20:37:01 +0300

    add cycle code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+
+from pygame.display import update
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -11,15 +14,25 @@ icon = pygame.image.load("img/tir2.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/tir3.jpg")
-target_width = 80
-target_height = 80
+target_width = 20
+target_height = 20
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-
 runnig = True
 while runnig:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False              # окно не закрывается!
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x + target_width and target_y < mouse_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display,update()
 
 pygame.quit()
